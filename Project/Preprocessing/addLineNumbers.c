a2c16db79bdf298c47324f9d7529b32c28a3c428
ChandulaJ <cj@DESKTOP-2O7SN5A.localdomain>
2025-06-01 16:40:03 +0530

    preprocessing added

---
+++ Project/Preprocessing/addLineNumbers.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE 1024
+
+int main() {
+    char inputFileLocation[200] = "/home/cj/HPC_data/Human_genome.fna";
+    char outputFileLocation[200] = "/home/cj/HPC_data/Human_genome_preprocessed.fna";
+
+    FILE *infile = fopen(inputFileLocation, "r");
+    FILE *outfile = fopen(outputFileLocation, "w");
+
+    if (!infile || !outfile) {
+        perror("File open error");
+        return 1;
+    }
+
+    char line[MAX_LINE];
+    char chromosome[11] = "";
+    int line_num = 0;
+
+    while (fgets(line, sizeof(line), infile)) {
+        // Remove newline
+        line[strcspn(line, "\r\n")] = 0;
+
+        //when it is a chromosome header
+        if (line[0] == '>') {
+            printf("%s\n", line);
+            strncpy(chromosome, line + 1, sizeof(chromosome));
+            chromosome[sizeof(chromosome)-1] = '\0';  // Safe null-terminate
+            line_num = 0;  // Reset line count
+        } else if (strlen(line) > 0) {
+            line_num++;
+            // Format line number as 4-digit
+            fprintf(outfile, "%s_%010d|%s\n", chromosome, line_num, line);
+        }
+    }
+
+    fclose(infile);
+    fclose(outfile);
+    printf("Preprocessing complete. Output written to output.fna\n");
+
+    return 0;
+}
